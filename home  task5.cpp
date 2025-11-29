1eb379f51ff9aa50385963009fbea05459dca05a
Zakria suleman <uop5478@gmail.com>
2025-11-30 03:18:48 +0500

    Add files via upload

---
+++ home  task5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+          int x=25;
+          int y=35;
+          int z=45;
+          int sum,sub,mul,div;
+          sum=x+y+z;
+          sub=x-y-z;
+          mul=x*y*z;
+          div=x/y/z;
+          cout<<"sum="<<sum<<endl;
+          cout<<"sub="<<sub<<endl;
+          cout<<"mul="<<mul<<endl;
+          cout<<"div="<<div<<endl;
+          system("pause");
+          }
